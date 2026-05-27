9628d40be66c90e8663fa78c319de846d8e37f1a
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-05-27 22:03:35 +0700

    chore: move sitemap to public folder for auto deployment

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   siteUrl: 'https://kalanalabs.com',
   generateRobotsTxt: true,
-  outDir: 'out', // Tempat menyimpan sitemap.xml & robots.txt pasca static export
+  outDir: 'public', // Tempat menyimpan sitemap.xml & robots.txt pasca static export
   generateIndexSitemap: false, // Menghasilkan satu file sitemap saja, bukan sitemap-index
 }
